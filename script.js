3d373e0c2cc1f2fc902b1a3bf9830490fd0b72d8
RedInJapanese <abasu924@gmail.com>
2023-05-19 19:08:27 -0700

    update: script.js

--- script.js
+++ script.js
@@ -53,10 +53,75 @@ class Scene1 extends Phaser.Scene {
         super('Scene1');
     }
     preload(){
+        this.load.image('ball', './assets/Ball.png');
+        this.load.image('tile', './assets/block.jpg');
+
     }
     create(){
+        this.ACCELERATION = 500;
+        this.MAX_X_VEL = 500;   // pixels/second
+        this.MAX_Y_VEL = 5000;
+        this.DRAG = 600;    // DRAG < ACCELERATION = icy slide
+        this.JUMP_VELOCITY = -1000;
+        this.physics.world.gravity.y = 3000;
+
+        // set bg color
+        this.cameras.main.setBackgroundColor('#FFFFFF');
+        this.add.text(game.config.width/2, 300, 'Press f to switch scenes', { font: '40px Futura', fill: '#000000' }).setOrigin(0.5);
+
+        // draw grid lines for jump height reference
+
+        this.ball = this.physics.add.sprite(game.config.width/2, game.config.width/2, 'ball').setScale(0.1);
+
+        this.ground = this.add.group();
+        for(let i = 0; i < game.config.width; i += 5) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - 5, 'tile').setScale(10).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        for(let i = 5*7; i < game.config.width-5*4; i += 5) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - 5*5, 'tile').setScale(10).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        for(let i = 5*2; i < game.config.width-5*13; i += 5) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - 5*9, 'tile').setScale(10).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        this.ball.setMaxVelocity(this.MAX_X_VEL, this.MAX_Y_VEL);
+        this.ball.setCollideWorldBounds(true);
+        this.ball.setBounceY(1);
+
+        this.input.keyboard.on('keydown', (event) => {
+            if(event.key == 'f'){
+                    this.scene.start('Scene2')
+            }
+        })
+
     }
-    update(){}
+    update(){
+        let cursors = this.input.keyboard.createCursorKeys();
+
+        // check keyboard input
+        if(cursors.left.isDown) {
+            this.ball.body.setAccelerationX(-this.ACCELERATION);
+            this.ball.setFlip(true, false);
+
+        } else if(cursors.right.isDown) {
+            this.ball.body.setAccelerationX(this.ACCELERATION);
+            this.ball.setFlip(true, false);        
+        } 
+        else {
+            this.ball.body.setAccelerationX(0);
+            this.ball.body.setDragX(this.DRAG);        
+        }
+        this.physics.world.wrap(this.ball, this.ball.width/2);
+
+    }
 }
 
 
@@ -102,7 +167,7 @@ class Logo extends Phaser.Scene {
         })
     }
     update(){
-        
+
     }
 }
 
@@ -112,8 +177,15 @@ let config = {
     type: Phaser.AUTO,
     width: 1200,
     height: 1000,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: [
-        Logo
+        Scene1
     ]
 }
 
